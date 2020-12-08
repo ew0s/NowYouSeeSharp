2e57e982e7c1b38438d3a867d95754b35465a769
ew0s <ew0s@DESKTOP-LSEJNSL>
2020-12-08 21:26:14 +0300

    Add: base context of application

---
+++ CourseWork/EncryptApp/EncryptApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using EncryptApp.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace EncryptApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        IWebHostEnvironment _appEnvironment;
+ 
+        public HomeController(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+ 
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddFile(IFormFile uploadedFile)
+        {
+            if (uploadedFile == null) return RedirectToAction("Index");
+            var path = "/Files/" + uploadedFile.FileName;
+            await using var fileStream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create);
+            await uploadedFile.CopyToAsync(fileStream);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CourseWork/EncryptApp/EncryptApp/Models/FileModel.cs
@@ -0,0 +1,9 @@
+namespace EncryptApp.Models
+{
+    public class FileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
